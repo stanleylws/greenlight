d40bbcf05a8db72a08968b4b4a419b02642d2da9
stanleylws <stanley.lws10@gmail.com>
2024-11-30 19:59:29 +0800

    Use httprouter and add create and view movie handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -10,3 +10,17 @@ func (app *application) healtcheckHandler(w http.ResponseWriter, r *http.Request
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
 }
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show details of movie %d", id)
+}
