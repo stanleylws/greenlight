e9c71bd7f1607727c2be160af172ee078e8a2c9b
Stanley Lee <sl@topo.cc>
2024-12-01 13:52:33 +0800

    Envelope json response

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,20 +9,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var ErrInvalidIdParam = errors.New("invalid id parameter")
+type envelope map[string]any
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
-		return 0, ErrInvalidIdParam
+		return 0, errors.New("invalid id parameter")
 	}
 
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.Marshal(data)
 	if err != nil {
 		return err
